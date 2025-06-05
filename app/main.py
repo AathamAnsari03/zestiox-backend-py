94e75831a605e9942d2a148e1ce121d1cde5d6ad
Senthamil143 <senthamil0214@gmail.com>
2025-06-05 21:14:29 +0530

    updated profile component

--- app/main.py
+++ app/main.py
@@ -1,10 +1,17 @@
 from flask import Flask
+from flask_cors import CORS
+
+
+
 from app.api.employee import employee_bp
+from app.api.profile import profile_bp
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
 # Register blueprints
 app.register_blueprint(employee_bp)
+app.register_blueprint(profile_bp)
 
 if __name__ == "__main__":
     app.run(debug=True)
