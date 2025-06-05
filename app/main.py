6d6ce07db7af2822c0d30fd7594555f3634983c4
AathamAnsari03 <ansariseenu84@gmail.com>
2025-06-05 20:23:08 +0530

    integrate frontend and backend

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from app.api.employee import employee_bp
+from app.api.order import order_bp
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 # Register blueprints
 app.register_blueprint(employee_bp)
+app.register_blueprint(order_bp)
 
 if __name__ == "__main__":
     app.run(debug=True)
