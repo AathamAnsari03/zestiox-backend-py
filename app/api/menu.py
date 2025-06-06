8a9ea309f024365ecbbce463a585c98e308d539b
Kathir-24 <kathiriperumal24@gmail.com>
2025-06-06 21:41:45 +0530

    fixed user id as dynamic

--- app/api/menu.py
+++ app/api/menu.py
@@ -7,7 +7,7 @@ def get_db_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="Hemanagaraj@11",
+        password="root",
         database="zestiox"
     )
 
