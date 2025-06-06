48f4aacfd40aa2ebd4eeb7db9e7d080f906e0408
AathamAnsari03 <ansariseenu84@gmail.com>
2025-06-06 22:23:54 +0530

    updated dynamic user

--- app/api/auth.py
+++ app/api/auth.py
@@ -9,7 +9,7 @@ def get_db_connection():
     return pymysql.connect(
         host='localhost',
         user='root',
-        password='root',  # <-- change this
+        password='Ansari@2004',  # <-- change this
         db='zestiox',
         cursorclass=pymysql.cursors.DictCursor
     )
