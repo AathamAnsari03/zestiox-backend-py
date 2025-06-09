f5176ba0ba356ce48a696cea223409cf8966591d
Senthamil143 <senthamil0214@gmail.com>
2025-06-09 09:11:24 +0530

    Implemented dynamic user

--- app/db/database.py
+++ app/db/database.py
@@ -4,6 +4,6 @@ from flask import Flask
 db = SQLAlchemy()
 
 def init_db(app: Flask):
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:abinesh@localhost/zestiox'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Senthamil%4014@localhost/zestiox'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
